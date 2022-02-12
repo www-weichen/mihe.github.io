3d80dcb6281fed0fe99de9f700b19e47375f9188
www-weichen <903717711@qq.com>
2022-02-12 21:50:11 +0800

    Add files via upload

---
+++ mlg/js/config.js
@@ -0,0 +1,13 @@
+// 计算当前html的font-size值
+(function (doc, win) { 
+    var docEl = doc.documentElement, 
+    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize', 
+    recalc = function () { 
+      var clientWidth = docEl.clientWidth; 
+      if (!clientWidth) return; 
+      docEl.style.fontSize = 100 * (clientWidth / 375) + 'px'; 
+    }; 
+    if (!doc.addEventListener) return; 
+       win.addEventListener(resizeEvt, recalc, false); 
+       doc.addEventListener('DOMContentLoaded', recalc, false); 
+})(document, window);  
